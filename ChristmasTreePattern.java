6c622c48910cced5b729da1cc07949b85f17f8f3
narmathagn <42265938+narmathagn@users.noreply.github.com>
2020-08-09 00:51:06 -0700

    Update ChristmasTreePattern.java

--- ChristmasTreePattern.java
+++ ChristmasTreePattern.java
@@ -3,15 +3,15 @@ public class ChristmasTreePattern{
 	public static void main(String args[]){
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the no of days:");
-		int day=s.nextInt();
-		int n=day+1;
-		int space=day;
+		int days=s.nextInt();
+		int n=days+1;
+		int space=days;
 		int star=1;
-		if(day<1)
+		if(days<1)
 		{
 			System.out.println("Tree haven't grown up");	
 		}
-		else if(day>=20)
+		else if(days>=20)
 		{
 			System.out.println("Tree haven't grown up");	
 		}
@@ -31,9 +31,9 @@ public class ChristmasTreePattern{
 			System.out.println();
 		}
 		n=n-2;
-		for(int i=0;i<day-2;i++)
+		for(int i=0;i<days-2;i++)
 		{
-		    space=day-1;
+		    space=days-1;
 			star=3;
 			for(int l=0;l<n;l++)
 			{
@@ -53,7 +53,7 @@ public class ChristmasTreePattern{
 		}
 		for(int i=0;i<2;i++)
 		{
-			for(int j=0;j<day;j++)
+			for(int j=0;j<days;j++)
 			{
 				System.out.print(" ");
 			}
